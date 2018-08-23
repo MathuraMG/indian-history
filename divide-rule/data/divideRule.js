a1280f06cf6ca57f5dda3dfb4d7eafad9af5cb86
harimohanraj89 <hari.mohanraj89@gmail.com>
2018-08-23 16:22:55 -0400

    Add pages dynamically

---
+++ divide-rule/data/divideRule.js
@@ -0,0 +1,19 @@
+window.data = window.data || {};
+
+window.data.divideRule = [
+  {
+    imageUrl: "images/1765FullTimeline.png"
+  },
+  {
+    imageUrl: "images/1795FullTimeline.png"
+  },
+  {
+    imageUrl: "images/1805FullTimeline.png"
+  },
+  {
+    imageUrl: "images/1823FullTimeline.png"
+  },
+  {
+    imageUrl: "images/1848FullTimeline.png"
+  }
+];
--- divide-rule/js/app.js
+++ divide-rule/js/app.js
@@ -11,22 +11,50 @@ var renderPage = function(pageNumber) {
 
 var hideAllMaps = function() {
   maps.addClass("hidden");
-}
+};
 
 var showMaps = function(pageNumber) {
   maps.each(function(map) {
     var map = $(this);
-    
+
     if (parseInt(map.data("page")) <= pageNumber) {
       map.removeClass("hidden");
     }
   });
+};
+
+var Timeline = function(opts) {
+  this.project = opts.project;
+  this.mapsEl = opts.mapsEl;
+  this.pagesEl = opts.pagesEl;
+};
+
+Timeline.prototype.init = function() {
+  var data = window.data[this.project];
+
+  data.forEach(this.addPage.bind(this));
+};
+
+Timeline.prototype.addPage = function(page, index) {
+  if (page.imageUrl) {
+    var pageEl = $("<div>").addClass("page").data("page", index);
+
+    this.pagesEl.append(pageEl);
+  }
 }
 
 $(function() {
+  var timeline = new Timeline({
+    project: "divideRule",
+    mapsEl: $(".view"),
+    pagesEl: $(".pages")
+  });
+
+  timeline.init();
+
   maps = $(mapSelector);
   pages = $(pageSelector);
-  
+
   $.scrollify({
     section : pageSelector,
     before: renderPage
