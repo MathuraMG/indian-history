f8115348be1fa8efc3b14cc42c1862365d8b9175
harimohanraj89 <hari.mohanraj89@gmail.com>
2018-08-23 16:00:44 -0400

    Do not capture _all_ key events, just the ones we care about

--- divide-rule/js/keyboard.js
+++ divide-rule/js/keyboard.js
@@ -1,20 +1,20 @@
 var keymap = {
   74: "j",
-  75: "k" 
+  75: "k"
 }
 
 var handleKeydown = function(event) {
   switch (keymap[event.which]) {
     case "j":
     $.scrollify.next();
+    event.preventDefault();
     break;
-    
+
     case "k":
     $.scrollify.previous();
+    event.preventDefault();
     break;
   }
-  
-  event.preventDefault();
 };
 
 $(function() {
