3b59850202598a44eef974cc95dfdee954e28227
harimohanraj89 <hari.mohanraj89@gmail.com>
2018-08-23 16:42:19 -0400

    Introduce a Timeline class to manage maps and pages

--- divide-rule/js/app.js
+++ divide-rule/js/app.js
@@ -1,48 +1,3 @@
-var mapSelector = ".map";
-var pageSelector = ".page";
-
-var maps;
-var pages;
-
-var renderPage = function(pageNumber) {
-  hideAllMaps();
-  showMaps(pageNumber);
-};
-
-var hideAllMaps = function() {
-  maps.addClass("hidden");
-};
-
-var showMaps = function(pageNumber) {
-  maps.each(function(map) {
-    var map = $(this);
-
-    if (parseInt(map.data("page")) <= pageNumber) {
-      map.removeClass("hidden");
-    }
-  });
-};
-
-var Timeline = function(opts) {
-  this.project = opts.project;
-  this.mapsEl = opts.mapsEl;
-  this.pagesEl = opts.pagesEl;
-};
-
-Timeline.prototype.init = function() {
-  var data = window.data[this.project];
-
-  data.forEach(this.addPage.bind(this));
-};
-
-Timeline.prototype.addPage = function(page, index) {
-  if (page.imageUrl) {
-    var pageEl = $("<div>").addClass("page").data("page", index);
-
-    this.pagesEl.append(pageEl);
-  }
-}
-
 $(function() {
   var timeline = new Timeline({
     project: "divideRule",
@@ -50,13 +5,8 @@ $(function() {
     pagesEl: $(".pages")
   });
 
-  timeline.init();
-
-  maps = $(mapSelector);
-  pages = $(pageSelector);
-
   $.scrollify({
-    section : pageSelector,
-    before: renderPage
+    section : ".page",
+    before: timeline.renderPage.bind(timeline)
   });
 });
